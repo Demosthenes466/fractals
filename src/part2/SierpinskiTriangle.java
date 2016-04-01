045630931b8a289dff8b9bd90221bf35bac4318c
Tannor Ziehm <ziehmtj@s.dcsdk12.org>
2016-04-01 09:54:44 -0600

    I did the part 2

--- src/part2/SierpinskiTriangle.java
+++ src/part2/SierpinskiTriangle.java
@@ -22,7 +22,31 @@ public class SierpinskiTriangle {
 	}
    
 	public void sierpinski(Point2D.Double a, Point2D.Double b, Point2D.Double c, int n) {
-	   // TODO: implement
+		Point2D.Double newA = new Point2D.Double(Math.abs((c.x+a.x)/2), Math.abs((c.y+a.y)/2));
+		Point2D.Double newB = new Point2D.Double(Math.abs((a.x+b.x)/2), Math.abs((a.y+b.y)/2));
+		Point2D.Double newC = new Point2D.Double(Math.abs((b.x+c.x)/2), Math.abs((b.y+c.y)/2));
+
+		   drawTriangle(a, b, c, StdDraw.BLACK);
+		   n--;
+		if (n <= 0 ) {
+		   drawTriangle(a, newA, newB, Color.WHITE);   
+		   drawTriangle(newA, newC, c, Color.WHITE);
+		   drawTriangle(newB, b, newC, Color.WHITE);
+		} else {
+			sierpinski(a, newA, newB, n); 
+			sierpinski(newA, newC, c, n);
+			sierpinski(newB, b, newC, n);
+		}
+		
+		
+	   
+	
+
+
+
+
+	   System.out.println(newC);
+
     }
    
 	public void drawTriangle(Point2D.Double a, Point2D.Double b, Point2D.Double c, Color color) {
@@ -40,6 +64,6 @@ public class SierpinskiTriangle {
 	
 	public static void main(String[] args) { 
 		SierpinskiTriangle triangle = new SierpinskiTriangle();
-		triangle.draw(3);
+		triangle.draw(100);
 	}
 }
